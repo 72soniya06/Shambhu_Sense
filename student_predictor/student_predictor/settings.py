6cceb202a820db6d4eea38ed158931e3676a601b
72soniya06 <soniyasri79@gmail.com>
2025-11-04 21:31:06 +0530

    recently updated

--- student_predictor/student_predictor/settings.py
+++ student_predictor/student_predictor/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'student_predictor.urls'
 
 TEMPLATES = [
